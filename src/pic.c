91f4928eefd2b68581ae94c2cb5b7c6b8ea13930
Phil Cowans <phil@philcowans.com>
2011-12-27 14:01:58 +0000

    .

--- src/pic.c
+++ src/pic.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+void calculate_electic_field(N, phi, E);
+void calculate_electron_density(N, r, ne);
+void calculate_potential(N, phi, rho, kappa);
 double distribution(double vb); // Sample from velocity distribution of beam
+void evolve_solution(double &t, int N, double *r, double *v, void (*rhs_eval)(double &, int, double *, double *, double *, double *), double dt); // Evolve solution by one timestep
+void normalize_coordinates(int N, double *r, double *v);
+void rhs_eval(double &t, int N, int J, double *r, double *v, double dt); // calculate derivative for evolution proces
 
 int main(int argc, char **argv) {
   
@@ -28,6 +34,14 @@ int main(int argc, char **argv) {
     v[i] = distribution(vb);
   }
 
+  // Now actually run it
+
+  int num_steps = (int)floor(tmax / dt);
+  for(int i = 0; i < num_steps; ++i) {
+    evolve_solution(t, N, r, v, rhs_eval, dt);
+    normalize_coordinates(N, r, v);
+  }
+
   return 0;
 }
 
@@ -48,4 +62,51 @@ double distribution(double vb) {
     return distribution (vb);
   else 
     return v;
+}
+
+// Numerical integrator
+
+void evolve_solution(double &t, int N, double *r, double *v, void (*rhs_eval)(double, int, double *, double *, double *, double *), double dt) {
+  
+}
+
+void normalize_coordinates(int N, double *r, double *v) {
+  for(int i = 0; i < N; ++i) {
+    if(r[i] < 0.0) 
+      r[i] += L;
+    if(r[i] > L)
+      r[i] -= L;
+  }
+}
+
+// Calculate derivative
+
+void rhs_eval(double &t, int N, int J, double *r, double *v, double *rdot, double *vdot) {
+  normalize_coordinates(N, r, v);
+
+  double ne[N];
+  calculate_electron_density(N, r, ne);
+  
+  double n0 = (double)N/L;
+  double rho[J];
+  for(int j = 0; j < J; ++j)
+    rho[J] = ne[j] / n0 - 1.0;
+  double kappa = 2.0 * M_PI / L;
+  double phi[N];
+  calculate_potential(N, phi, rho, kappa);
+
+  double E[N];
+  calculate_electic_field(N, phi, E);
+
+  for(int i = 0; i < N; ++i) {
+    double dx = L / (double)J;
+    int j = (int)(floor(r[i]/dx));
+    double y = r[i]/dx - (double)j;
+    
+    double Efield = E[j] * (1.0 - y) + E[((j+1) == J) ? 0 : j + 1] * y;
+    
+    rdot[i] = v[i];
+    vdot[i] = -Efield;
+  }
+  
 }
